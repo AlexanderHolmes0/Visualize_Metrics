0cb7e14f3b18597e94c75e742041ef8eefe20d09
Alexander Holmes <108624793+AlexanderHolmes0@users.noreply.github.com>
2024-01-16 23:45:11 -0500

    Simulate Prob

--- Visualize_Metrics/app.R
+++ Visualize_Metrics/app.R
@@ -1,6 +1,7 @@
 library(shiny)
 library(caret)
 library(pROC)
+library(shinyWidgets)
 
 source('Helpers.R')
 
@@ -18,6 +19,20 @@ ui <- fluidPage(
         numericInput("fp", "False Positives", 15),
         numericInput("tn", "True Negatives", 30),
         numericInput("fn", "False Negatives", 15),
+        prettySwitch(
+            inputId = "simulate",
+            label = "Simulate Probabilities", 
+            status = "success",
+            value = FALSE,
+            fill = TRUE
+        ),
+        prettySwitch(
+            inputId = "smooth",
+            label = "Smooth ROC Curve", 
+            status = "success",
+            value = FALSE,
+            fill = TRUE
+        ),
         width = 2
         ),
 
@@ -53,18 +68,28 @@ server <- function(input, output) {
                  "Please Enter Non-negative Numbers")
         )
         
-        
+        if(input$simulate==FALSE){
         FundTest <- data.frame(Target_B = c(rep(0,input$tn),
-                                          rep(1,input$tp),
-                                          rep(1,input$fn),
-                                          rep(0,input$fp)),
-                             predict1 = c(rep(0,input$tn),
-                                          rep(1,input$tp),
-                                          rep(0,input$fn),
-                                          rep(1,input$fp)))
-       
+                                            rep(1,input$tp),
+                                            rep(1,input$fn),
+                                            rep(0,input$fp)),
+                             predict1 =   c(rep(0,input$tn),
+                                            rep(1,input$tp),
+                                            rep(0,input$fn),
+                                            rep(1,input$fp)))
+        }else{
+        FundTest <- data.frame(Target_B = c(rep(0,input$tn),
+                                            rep(1,input$tp),
+                                            rep(1,input$fn),
+                                            rep(0,input$fp)),
+                               predict1 = c(pnorm(sample(seq(0,.5,length=100),input$tn,replace=T)),
+                                            pnorm(sample(seq(.5,1,length=100),input$tp,replace=T)),
+                                            pnorm(sample(seq(0,.5,length=100),input$fn,replace=T)),
+                                            pnorm(sample(seq(.5,1,length=100),input$fp,replace=T))))
+        }
+        
         rocCurve <- roc(response = FundTest$Target_B, predictor = FundTest$predict1)
-        plot(rocCurve,
+        plot(if(input$simulate==FALSE) rocCurve else if(input$smooth == TRUE) {smooth(rocCurve)} else {rocCurve},
              identity=TRUE, 
              legacy.axes=T, 
              col = "blue", 
